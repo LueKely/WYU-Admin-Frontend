7d890441118824ce85ce16f60a9c606917e2e63b
LueKely <99306129+LueKely@users.noreply.github.com>
2024-01-17 12:30:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+	<nav>
+		<RouterLink to="/">Dashboard</RouterLink>
+	</nav>
+
+	<RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+	<main>TEST</main>
+</template>
